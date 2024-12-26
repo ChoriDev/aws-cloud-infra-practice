a5c89c4bbd0bc465247f5dce18e88df1eaea1a84
Chori <yidodeveloper@gmail.com>
2024-12-26 14:16:09 +0900

    🔒 Allow the public to read index page

--- serverless-web-hosting/s3.tf
+++ serverless-web-hosting/s3.tf
@@ -13,3 +13,28 @@ resource "aws_s3_bucket_website_configuration" "web_hosting" {
     suffix = "mycar.html"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.main.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "PublicReadGetObject",
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.main.arn}/*"
+      }
+    ]
+  })
+}

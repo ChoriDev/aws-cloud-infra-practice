bc247304d2235963beadf921911afc947eb26d28
Chori <yidodeveloper@gmail.com>
2024-12-28 15:33:42 +0900

    🎉 Initialize backend, provider and vpc for LAMP

---
+++ LAMP-web-server/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "aws-cloud-infra-practice-tfstate"
+    key = "workspace/LAMP-web-server/terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "terraform-lock"
+  }
+}

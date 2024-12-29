570cf5e7b7c8fbc72982e140271078bf49a2c5ff
Chori <yidodeveloper@gmail.com>
2024-12-29 15:35:16 +0900

    ✨ Create security group for public subnet

---
+++ LAMP-web-server/security_group.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "public_ec2_sg" {
+  name = "public-ec2-sg"
+  description = "Security group for public ec2 instance"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-ec2-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  ip_protocol = "tcp"
+  to_port = 443
+}

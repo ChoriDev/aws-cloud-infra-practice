eb32639d7705fe1f41060baa7bbe3a082eb68a24
Chori <yidodeveloper@gmail.com>
2024-12-31 11:32:57 +0900

    ✨ Create EFS to be mounted on EC2 instance

---
+++ LAMP-web-server/efs.tf
@@ -0,0 +1,34 @@
+# EFS
+resource "aws_efs_file_system" "main" {
+  performance_mode = "generalPurpose"
+  throughput_mode = "bursting"
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  lifecycle_policy {
+    transition_to_primary_storage_class = "AFTER_1_ACCESS"
+  }
+
+  tags = {
+    Name = "efs-${var.vpc_name}"
+  }
+}
+
+# EFS backup policy
+resource "aws_efs_backup_policy" "efs_backup_policy" {
+  file_system_id = aws_efs_file_system.main.id
+
+  backup_policy {
+    status = "DISABLED"
+  }
+}
+
+# EFS mount target
+resource "aws_efs_mount_target" "public_subnet" {
+  count = length(var.cidr_numeral_public)
+  file_system_id = aws_efs_file_system.main.id
+  subnet_id = element(aws_subnet.public.*.id, count.index)
+  security_groups = [ aws_security_group.efs_sg.id ]
+}
--- LAMP-web-server/security_group.tf
+++ LAMP-web-server/security_group.tf
@@ -36,9 +36,37 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   to_port = 443
 }
 
-# Outbound rule allowing all traffic
-resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic" {
+# Outbound rule allowing all traffic for ec2
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic_for_ec2" {
   security_group_id = aws_security_group.public_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+# Security group for EFS
+resource "aws_security_group" "efs_sg" {
+  name = "efs-sg"
+  description = "Security group for efs"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "efs-sg"
+  }
+}
+
+# Inbound rule allowing EFS
+resource "aws_vpc_security_group_ingress_rule" "allow_efs" {
+  security_group_id = aws_security_group.efs_sg.id
+  # Fill in below value with security group whose instance will connect to EFS
+  referenced_security_group_id = aws_security_group.public_ec2_sg.id
+  from_port = 2049
+  ip_protocol = "tcp"
+  to_port = 2049
+}
+
+# Outbound rule allowing all traffic for EFS
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic_for_efs" {
+  security_group_id = aws_security_group.efs_sg.id
   cidr_ipv4 = "0.0.0.0/0"
   ip_protocol = "-1"
 }

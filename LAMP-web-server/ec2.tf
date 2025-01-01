697e2a92176dd919910f36c2f87abd3a9d4840a0
Chori <yidodeveloper@gmail.com>
2025-01-02 08:19:12 +0900

    ✨ Create four private ec2 instances

--- LAMP-web-server/ec2.tf
+++ LAMP-web-server/ec2.tf
@@ -3,12 +3,47 @@ resource "aws_instance" "public_ec2" {
   count = length(var.cidr_numeral_public)
   ami = "ami-048c8b90bfe9b49b8" # Amazon Linux 2, x86
   instance_type = "t2.micro"
-  key_name = aws_key_pair.ec2_key_pair.key_name
+  key_name = aws_key_pair.public_ec2_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.public_ec2_sg.id]
   subnet_id = element(aws_subnet.public.*.id, count.index)
 
   tags = {
     Name = "public-ec2-${count.index}-${var.vpc_name}"
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_put_response_hop_limit = 1
+    http_tokens = "optional"
+    instance_metadata_tags = "enabled"
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+    yum install -y httpd mariadb-server
+    systemctl start httpd
+    systemctl enable httpd
+    usermod -a -G apache ec2-user
+    chown -R ec2-user:apache /var/www
+    chmod 2775 /var/www
+    find /var/www -type d -exec chmod 2775 {} \;
+    find /var/www -type f -exec chmod 0664 {} \;
+  EOF
+}
+
+# Private ec2
+resource "aws_instance" "private_ec2" {
+  count = length(var.cidr_numeral_private)
+  ami = "ami-048c8b90bfe9b49b8" # Amazon Linux 2, x86
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.private_ec2_key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.private_ec2_sg.id]
+  subnet_id = element(aws_subnet.private.*.id, count.index)
+
+  tags = {
+    Name = "private-ec2-${count.index}-${var.vpc_name}"
   }
 
   metadata_options {
--- LAMP-web-server/security_group.tf
+++ LAMP-web-server/security_group.tf
@@ -43,6 +43,51 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic_for_pu
   ip_protocol = "-1"
 }
 
+# Security group for private ec2
+resource "aws_security_group" "private_ec2_sg" {
+  name = "private-ec2-sg"
+  description = "Security group for private ec2 instance"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-ec2-sg"
+  }
+}
+
+# Inbound rule allowing SSH for private ec2
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_for_private_ec2" {
+  security_group_id = aws_security_group.private_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+# Inbound rule allowing HTTP for private ec2
+resource "aws_vpc_security_group_ingress_rule" "allow_http_for_private_ec2" {
+  security_group_id = aws_security_group.private_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+}
+
+# Inbound rule allowing HTTPS for private ec2
+resource "aws_vpc_security_group_ingress_rule" "allow_https_for_private_ec2" {
+  security_group_id = aws_security_group.private_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  ip_protocol = "tcp"
+  to_port = 443
+}
+
+# Outbound rule allowing all traffic for private ec2
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic_for_private_ec2" {
+  security_group_id = aws_security_group.private_ec2_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
 # Security group for EFS
 resource "aws_security_group" "efs_sg" {
   name = "efs-sg"

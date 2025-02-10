06e0c9e95acdaeadbe4fbd9cb46584ce93be64aa
Chori <yidodeveloper@gmail.com>
2025-02-10 22:39:45 +0900

    🎨 Fornat code

--- backend-s3/backend.tf
+++ backend-s3/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "aws-cloud-infra-practice-tfstate"
-    key = "workspace/backend-s3/terraform.tfstate"
-    region = "ap-northeast-2"
-    encrypt = true
+    bucket         = "aws-cloud-infra-practice-tfstate"
+    key            = "workspace/backend-s3/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
     dynamodb_table = "terraform-lock"
   }
 }
--- backend-s3/danamodb_table.tf
+++ backend-s3/danamodb_table.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "name" {
-  name = "terraform-lock"
-  hash_key = "LockID"
-  billing_mode =  "PAY_PER_REQUEST"
-  
+  name         = "terraform-lock"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
   attribute {
     name = "LockID"
     type = "S"
